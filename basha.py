8cbffde459dbc6dac7afa8d12c620846e8652342
shaikshahulhameed69 <shaikshahulhameed69@gmail.com>
2026-09-03 15:26:07 +0530

    hhhhh

--- basha.py
+++ basha.py
@@ -2,9 +2,9 @@ a = 10
 b = 20
 c = 6
 
-password = "shahulgaggjj@hhddh"
+password = "shahulgaggjj@hhddh124522"
 
-sum = a + b + c + len(password)
+sum = a + b + c + password
 
 
 print("Sum =", sum)
